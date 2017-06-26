627219a922082840e8069838a11b18d4fb139ea0
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2017-06-25 21:55:09 -0400

    enabled circuit breaker and retry

--- src/main/java/com/example/GoogleReverseGeolookupSpringServiceApplication.java
+++ src/main/java/com/example/GoogleReverseGeolookupSpringServiceApplication.java
@@ -2,9 +2,13 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.retry.annotation.EnableRetry;
 
 @EnableFeignClients
+@EnableRetry
+@EnableCircuitBreaker
 @SpringBootApplication
 public class GoogleReverseGeolookupSpringServiceApplication {
 
