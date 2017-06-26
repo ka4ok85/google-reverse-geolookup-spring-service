2ec9ea347596fa64a281c780feac0ede52105ef6
Evgeny @ Home <makovetsky.evgeny@gmail.com>
2017-06-25 21:33:41 -0400

    added new exception class

---
+++ src/main/java/com/example/exceptions/ReverseGeoLookupResourceException.java
@@ -0,0 +1,7 @@
+package com.example.exceptions;
+
+public class ReverseGeoLookupResourceException extends RuntimeException {
+
+	private static final long serialVersionUID = 8521936678133373320L;
+
+}
