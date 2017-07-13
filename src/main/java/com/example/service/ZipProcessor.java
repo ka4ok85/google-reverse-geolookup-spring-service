173008cb1fce45d4b29104425b96738c00748bff
ka4ok85 <makovetsky.evgeny@gmail.com>
2017-07-13 12:27:22 -0400

    switched to loop through all result arrays instead of relying on first

--- src/main/java/com/example/service/ZipProcessor.java
+++ src/main/java/com/example/service/ZipProcessor.java
@@ -13,28 +13,28 @@ import org.springframework.stereotype.Component;
 import com.example.dto.Call;
 import com.example.dto.GoogleGeoCodeResponse;
 import com.example.dto.GoogleGeoCodeResponseAddressComponent;
+import com.example.dto.GoogleGeoCodeResponseResult;
 
 @EnableBinding(Processor.class)
 @Component
 public class ZipProcessor {
 
 	private static final Logger log = LoggerFactory.getLogger(ZipProcessor.class);
-	
+
 	private static final String zipWord = "postal_code";
 
 	@Value("${google.appKey}")
 	private String appKey;
-	
-    @Value("${service.name}")
-    private String serviceName;
-	
+
+	@Value("${service.name}")
+	private String serviceName;
+
 	@Autowired
 	private HystrixReverseGeoLookupService hystrixReverseGeoLookupService;
-	
 
 	@StreamListener(Processor.INPUT)
-    @SendTo(Processor.OUTPUT)
-    public Call addZipToTheCall(Call call) { // enrich Call object with Zip Code
+	@SendTo(Processor.OUTPUT)
+	public Call addZipToTheCall(Call call) { // enrich Call object with Zip Code
 		if (call.getLatitude() == 0.0f && call.getLongitude() == 0.0f) {
 			log.warn("Service: {}. Incident: {}. Blank coordinates.", serviceName, call.getIncidentNumber());
 			return call;
@@ -43,13 +43,18 @@ public class ZipProcessor {
 		GoogleGeoCodeResponse response = hystrixReverseGeoLookupService.getZip(String.valueOf(call.getLatitude()) + "," + String.valueOf(call.getLongitude()), appKey, call.getIncidentNumber());
 		String zip = "";
 		if (response != null && response.getResults().length > 0) {
-			for (GoogleGeoCodeResponseAddressComponent addressComponent : response.getResults()[0].getAddressComponents()) {
-				if (addressComponent.getTypes()[0].equals(zipWord)) {
-					zip = addressComponent.getShortName();
+			resultsTopLevelLoop: for (GoogleGeoCodeResponseResult resultItem : response.getResults()) {
+				for (GoogleGeoCodeResponseAddressComponent addressComponent : resultItem.getAddressComponents()) {
+					if (addressComponent.getTypes()[0].equals(zipWord)) {
+						zip = addressComponent.getShortName();
+						if (zip.isEmpty() == false) {
+							break resultsTopLevelLoop;
+						}
+					}
 				}
 			}
 		}
-			
+
 		if (zip.isEmpty()) {
 			log.error("Service: {}. Incident: {}. Can not lookup Zip!", serviceName, call.getIncidentNumber());
 			log.error(response.toString());
@@ -58,5 +63,5 @@ public class ZipProcessor {
 			call.setZip(zip);
 			return call;
 		}
-    }
+	}
 }
